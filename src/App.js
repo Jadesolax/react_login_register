0292e65d9e7fbaca423ea1aa1dab396ac3340f8a
Olu <theoafactor@gmail.com>
2022-05-18 15:26:43 +0100

    New update with persistence

--- src/App.js
+++ src/App.js
@@ -6,21 +6,49 @@ import Register from './Components/Register/Register';
 
 class App extends React.Component{
 
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+
+    
+    //get the options
+    let current_page = localStorage.getItem("current-page");
+
+    let selectOptions;
+    if(current_page === "login"){
+      console.log("This is login")
+        selectOptions = <React.Fragment>
+                          <option selected>Select Page</option>
+                          <option value='register'>Register</option>
+                          <option value='login' selected>Log in</option>
+                        </React.Fragment>
+    }else{
+      console.log("This is register")
+      selectOptions = <React.Fragment>
+                        <option>Select Page</option>
+                        <option value='register' selected>Register</option>
+                        <option value='login'>Log in</option>
+                    </React.Fragment>
+
+    }
+
+    
 
     //app state
     this.state = {
       default_page : localStorage.getItem("current-page"),
       is_user_logged_in: false,
-      is_client_verified: false
+      is_client_verified: false,
+      selectOptions: selectOptions
     }
+
   }
 
   loadPage = (pagename) =>{
       // this.setState({
       //   default_page: pagename
       // })
+
+      
 
       if(pagename === "register"){
         this.setState({
@@ -40,9 +68,50 @@ class App extends React.Component{
 
   }
 
+  changePage = (event) => {
+    let pagename = event.target.value
+
+    this.loadPage(pagename)
+    
+
+
+  }
+
   render(){
+    
 
     let current_page = this.state.default_page
+
+    console.log(current_page)
+
+                  
+    if(current_page === "login"){
+
+      console.log(this.state)
+
+      this.state.selectOptions = 
+                  <React.Fragment>
+                      <option>Select Page</option>
+                      <option value='register'>Register</option>
+                      <option value='login' selected>Log in</option>
+                  </React.Fragment>
+    }else{
+      console.log('We are here Register')
+      this.state.selectOptions = 
+      <React.Fragment>
+          <option>Select Page</option>
+          <option value='register' selected>Register</option>
+          <option value='login'>Log in</option>
+      </React.Fragment>
+
+
+    }
+
+  
+
+    let pageOptions =  <React.Fragment>
+                        {this.state.selectOptions}
+                      </React.Fragment>
     
     let displayPage;
     if(current_page == "login"){
@@ -55,6 +124,9 @@ class App extends React.Component{
     return (
       <div className="App">
         <header className="App-header">
+          <select onChange={(event) => this.changePage(event)}>
+              {pageOptions}
+          </select>
           <img src={logo} className="App-logo" alt="logo" />
               {displayPage}
         </header>
